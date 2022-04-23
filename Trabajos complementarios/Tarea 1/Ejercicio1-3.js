2383019b89ca1ba45bd42e4a7122c08a7b19c891
MunozLopezKelvin <90059668+MunozLopezKelvin@users.noreply.github.com>
2022-04-23 02:21:56 -0500

    Delete Trabajos complementarios/Tarea 1 directory

--- Trabajos complementarios/Tarea 1/Ejercicio1-3.js
+++
@@ -1,8 +0,0 @@
-const ComidaFavorita = [
-{Nombre: 'Viche', Ingredientes: 'Pescado, Maní, Camote, Choclo...', Tipo: 'Plato fuerte', Region: 'Costero', Acompanante: 'Arroz' },
-{Nombre: 'Ceviche', Ingredientes: 'Pescado, Limón, Sal, Tomate, Cebolla...', Tipo: 'Plato fuerte' , Region: 'Costero', Acompanante: 'Chifle'},
-{Nombre: 'Encebollado', Ingredientes: 'Pescado, Yuca, Cebolla, Cilantro...', Tipo: 'Plato fuerte', Region: 'Costero', Acompanante: 'Chifle '},
-];
-for (const comfav of ComidaFavorita) {
-    console.log(comfav);
-  }
