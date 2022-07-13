2b485cabab0fb9c7e9d4c400447db9820d6d5bfd
MunozLopezKelvin <e1315908671@live.uleam.edu.ec>
2022-07-13 16:24:06 -0500

    Semana14

---
+++ Prácticas en clases/Semana13SSR/crudssr/routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+const axios = require('axios');
+
+const httpAxios= axios.create({
+  baseURL:`http://localhost:2500/v1/sextoa/api`
+})
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  httpAxios.get('productos').then(respuesta=>{
+    //console.log(respuesta.data.productos);
+    res.render('index', { productos: respuesta.data.productos });
+  })
+});
+
+module.exports = router;
